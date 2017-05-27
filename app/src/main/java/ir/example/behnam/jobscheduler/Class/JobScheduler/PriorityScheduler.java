73a862d3f012b293b28cef1109cd2e6228cc2e0b
Behnam Mabodi <gpxchat@gmail.com>
2017-05-27 14:39:12 +0430

    + Implemented PriorityScheduler Algorithm

--- app/src/main/java/ir/example/behnam/jobscheduler/Class/JobScheduler/PriorityScheduler.java
+++ app/src/main/java/ir/example/behnam/jobscheduler/Class/JobScheduler/PriorityScheduler.java
@@ -1,6 +1,8 @@
 package ir.example.behnam.jobscheduler.Class.JobScheduler;
 
 import ir.example.behnam.jobscheduler.Class.JobScheduler.Interface.Job;
+import ir.example.behnam.jobscheduler.Class.JobScheduler.Job.NormalJob;
+import ir.example.behnam.jobscheduler.Class.JobScheduler.Job.PriorityJob;
 
 /**
  * Created by goldm on 25/05/2017.
@@ -10,11 +12,38 @@ public class PriorityScheduler extends Scheduler {
 
 
     public PriorityScheduler() {
-        super(EXECUTION_MODE_PREEMPTIVE);
+        super(EXECUTION_MODE_NON_PREEMPTIVE);
     }
 
     @Override
     protected Job scheduleNextJob() {
+        Job nextJob = findSmallestPriority();
+
+        if (nextJob != null && !nextJob.isFinished()) {
+            if (nextJob.getArrivalTime() <= mTime) {
+                return nextJob;
+            } else {
+                return new NormalJob("-", 0, 1);
+            }
+        }
         return null;
+    }
+
+
+    protected Job findSmallestPriority() {
+        PriorityJob smallestPriorityJob = null;
+        for (Job job : mJobList) {
+            if (!job.isFinished()) {
+                smallestPriorityJob = (PriorityJob) job;
+                break;
+            }
+        }
+
+        if (smallestPriorityJob != null) {
+            for (Job job : mJobList)
+                if (!job.isFinished() && ((PriorityJob) job).getPriority() < smallestPriorityJob.getPriority() && job.getArrivalTime() <= mTime)
+                    smallestPriorityJob = (PriorityJob) job;
+        }
+        return smallestPriorityJob;
     }
 }
